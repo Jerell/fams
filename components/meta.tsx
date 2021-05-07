c4ed28215f08d3b481fe9673c1cd82b09f8462ed
Jerell James <jerell1998@gmail.com>
2021-05-07 19:01:05 +0100

    icons

--- components/meta.tsx
+++ components/meta.tsx
@@ -19,7 +19,7 @@ const Meta = () => (
 			content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
 		/>
 		<link rel='apple-touch-icon' href='/images/icon-maskable-512.png' />
-		<link rel='icon' type='image/png' href='/images/favicon.png' />
+		<link rel='icon' type='image/png' href='/images/favicon.ico' />
 		<link rel='manifest' href='/manifest.json' />
 	</Head>
 )
