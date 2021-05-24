208a81ab1c6f1be0fef741e3907b37237f1a452b
Jerell James <jerell1998@gmail.com>
2021-05-24 19:24:22 +0100

    node and pipe definitions

---
+++ public/model/__tests__/pipe.ts
@@ -0,0 +1,103 @@
+import Pipe from '../pipe'
+import Node from '../node'
+
+describe('Constructor', () => {
+	it('should have a name', () => {
+		const pipe = new Pipe()
+		expect(pipe.name).toBeTruthy()
+	})
+
+	it('should have a length', () => {
+		const pipe = new Pipe()
+		expect(pipe.length).not.toBeUndefined()
+	})
+
+	it('should have a diameter', () => {
+		const pipe = new Pipe()
+		expect(pipe.diameter).not.toBeUndefined()
+	})
+
+	it('should have a mass flow rate', () => {
+		const pipe = new Pipe()
+		expect(pipe.massFlow).not.toBeUndefined()
+	})
+
+	it('should have an in pressure', () => {
+		const pipe = new Pipe()
+		expect(pipe.pressure.in).not.toBeUndefined()
+	})
+
+	it('should have an out pressure', () => {
+		const pipe = new Pipe()
+		expect(pipe.pressure.out).not.toBeUndefined()
+	})
+
+	it('should have a source node', () => {
+		const pipe = new Pipe()
+		expect(pipe.source).toBeInstanceOf(Node)
+	})
+
+	it('should have a destination node', () => {
+		const pipe = new Pipe()
+		expect(pipe.destination).toBeInstanceOf(Node)
+	})
+})
+
+describe('Pressure', () => {
+	it('should have an input pressure the same as the source node', () => {
+		const node = new Node()
+		const pipe = new Pipe()
+
+		node.pressure = 1
+		pipe.source = node
+
+		expect(pipe.pressure.in).toBe(1)
+	})
+
+	it('should update destination node pressure if out pressure is lower', () => {
+		const node = new Node()
+		const pipe = new Pipe()
+
+		node.pressure = 2
+		pipe.pressure.out = 1
+		pipe.destination = node
+
+		expect(node.pressure).toBe(1)
+	})
+
+	it('should calculate pressure drop', () => {
+		const node = new Node({ pressure: 1 })
+		const pipe = new Pipe({ length: 200, diameter: 2 })
+		pipe.source = node
+
+		const drop = pipe.pressureDrop()
+
+		expect(drop).toBeCloseTo(0.49)
+	})
+})
+
+describe('Direction', () => {
+	it('should return null by default', () => {
+		const pipe = new Pipe()
+		expect(pipe.direction).toBeNull()
+	})
+
+	it('should return true if the source pressure is greater than the destination pressure', () => {
+		const pipe = new Pipe()
+		const input = new Node({ pressure: 2 })
+
+		pipe.source = input
+
+		expect(pipe.direction).toBe(true)
+	})
+
+	it('should return false if the source pressure is less than the destination pressure', () => {
+		const pipe = new Pipe()
+		const input = new Node({ pressure: 1 })
+
+		pipe.source = input
+		pipe.pressure.out = 2
+
+		expect(pipe.direction).toBe(false)
+	})
+})
---
+++ public/model/pipe.ts
@@ -0,0 +1,79 @@
+import Node from './node'
+
+interface IPipe {
+	name?: string
+	length?: number
+	diameter?: number
+	massFlow?: number
+	source?: Node
+	destination?: Node
+}
+
+export default class Pipe {
+	name: string
+	length: number
+	diameter: number
+	massFlow: number
+	pressure: {
+		in: number
+		out: number
+	}
+	private _source: Node
+	_destination: Node
+
+	constructor(props: IPipe = {}) {
+		this.name = props.name || 'pipesection'
+		this.length = props.length || 200
+		this.diameter = props.diameter || 2
+		this.massFlow = props.massFlow || 0
+		this.pressure = {
+			in: 0,
+			out: 0,
+		}
+		this._source = new Node()
+		this._destination = new Node()
+
+		if (props.source) this.source = props.source
+		if (props.destination) this.destination = props.destination
+	}
+
+	pressureDrop(): number {
+		const P1 = this.pressure.in
+		const viscosity = 1 // fluid property
+		const L = this.length
+		const Q = 1 // volumetric flow rate
+		const A = 0.25 * Math.PI * this.diameter ** 2
+		const d = this.diameter * 1000 // mm
+		const z = this.destination.elevation - this.source.elevation
+		const g = 9.81
+		const density = 1
+
+		return P1 - (32000 * (viscosity * L * Q)) / (A * d ** 2) - z * g * density
+	}
+
+	set source(n: Node) {
+		this._source = n
+		this.pressure.in = n.pressure
+		this.pressure.out = this.pressure.in - this.pressureDrop()
+	}
+
+	get source() {
+		return this._source
+	}
+
+	set destination(n: Node) {
+		n.pressure = Math.min(this.pressure.out, n.pressure)
+		this.pressure.out = n.pressure
+		this._destination = n
+	}
+
+	get destination() {
+		return this._destination
+	}
+
+	get direction() {
+		if (this.pressure.in > this.pressure.out) return true
+		else if (this.pressure.in < this.pressure.out) return false
+		return null
+	}
+}
