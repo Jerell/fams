73884f507a448d15238f17237bd7739b5f1eac4a
Jerell James <jerell1998@gmail.com>
2021-06-16 14:19:39 +0100

    network with valve

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import NodeInput from '@/components/input/nodeInput'
 import NetworkMap from '@/components/networkMap'
 import Network from '@/public/model/network'
 
+// Network: basic
+
 const basicnet = new Network({ name: 'basic' })
 const B0 = basicnet.addNode({ pressure: 100000, temperature: 220 })
 const B1 = basicnet.addNode({ pressure: 100000, temperature: 220 })
@@ -15,38 +17,76 @@ const bP1 = basicnet.addPipe({
 bP1.source = B0
 bP1.destination = B1
 
-const net = new Network({ name: 'twojoin' })
-const N0 = net.addNode({ pressure: 100000, temperature: 220 })
-const N1 = net.addNode({ pressure: 100000, temperature: 220 })
-const N2 = net.addNode({ pressure: 100000, temperature: 220 })
-const N3 = net.addNode({ pressure: 10000, temperature: 220 })
+// Network: twojoin
 
-const P0 = net.addPipe({
+const tj = new Network({ name: 'twojoin' })
+const TJ0 = tj.addNode({ pressure: 100000, temperature: 220 })
+const TJ1 = tj.addNode({ pressure: 100000, temperature: 220 })
+const TJ2 = tj.addNode({ pressure: 100000, temperature: 220 })
+const TJ3 = tj.addNode({ pressure: 10000, temperature: 220 })
+
+const tjP0 = tj.addPipe({
 	length: 200,
 	diameter: 2,
 	massFlow: 10,
 	ignoreDestination: true,
 })
-P0.source = N0
-P0.destination = N1
+tjP0.source = TJ0
+tjP0.destination = TJ1
 
-const P1 = net.addPipe({
+const tjP1 = tj.addPipe({
 	length: 200,
 	diameter: 2,
 	massFlow: 10,
 	ignoreDestination: true,
 })
-P1.source = N1
-P1.destination = N2
+tjP1.source = TJ1
+tjP1.destination = TJ2
 
-const P2 = net.addPipe({
+const tjP2 = tj.addPipe({
 	length: 200,
 	diameter: 2,
 	massFlow: 10,
 	ignoreDestination: true,
 })
-P2.source = N3
-P2.destination = N1
+tjP2.source = TJ3
+tjP2.destination = TJ1
+
+// Network: twojoinvalve
+
+const tjv = new Network({ name: 'twojoinvalve' })
+const TJV0 = tjv.addNode({ pressure: 100000, temperature: 220 })
+const TJV1 = tjv.addNode({ pressure: 100000, temperature: 220 })
+const TJV2 = tjv.addNode({ pressure: 100000, temperature: 220 })
+const TJV3 = tjv.addNode({ pressure: 10000, temperature: 220 })
+
+const tjvP0 = tjv.addPipe({
+	length: 200,
+	diameter: 2,
+	massFlow: 10,
+	ignoreDestination: true,
+})
+tjvP0.source = TJV0
+tjvP0.destination = TJV1
+tjvP0.addValve()
+
+const tjvP1 = tjv.addPipe({
+	length: 200,
+	diameter: 2,
+	massFlow: 10,
+	ignoreDestination: true,
+})
+tjvP1.source = TJV1
+tjvP1.destination = TJV2
+
+const tjvP2 = tjv.addPipe({
+	length: 200,
+	diameter: 2,
+	massFlow: 10,
+	ignoreDestination: true,
+})
+tjvP2.source = TJV3
+tjvP2.destination = TJV1
 
 const Index = () => (
 	<Page>
@@ -58,7 +98,8 @@ const Index = () => (
 			{/* <NodeInput properties={['pressure', 'temperature', 'flow rate']} /> */}
 
 			<NetworkMap network={basicnet} />
-			<NetworkMap network={net} />
+			<NetworkMap network={tj} />
+			<NetworkMap network={tjv} />
 		</section>
 	</Page>
 )
