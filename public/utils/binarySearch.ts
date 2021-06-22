00b865f93e04c6cb7b82a5e20a48271bbd74f17f
Jerell James <jerell1998@gmail.com>
2021-06-22 16:49:48 +0100

    binary search nearest

---
+++ public/utils/binarySearch.ts
@@ -0,0 +1,28 @@
+export default function binarySearch(list: number[], item) {
+	let low = 0
+	let high = list.length - 1
+	let mid = 0
+	let best_index = low
+
+	while (low <= high) {
+		mid = Math.floor((low + high) / 2)
+
+		if (list[mid] < item) {
+			low = mid + 1
+		} else if (list[mid] > item) {
+			high = mid - 1
+		} else {
+			best_index = mid
+			break
+		}
+
+		const diff_b = Math.abs(list[best_index] - item)
+		const diff_m = Math.abs(list[mid] - item)
+
+		if ((best_index < mid && diff_m <= diff_b) || diff_m < diff_b) {
+			best_index = mid
+		}
+	}
+
+	return best_index
+}
