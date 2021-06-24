5ed87145bb808e76345fb069873f59e31cc30143
Jerell James <jerell1998@gmail.com>
2021-06-24 17:43:35 +0100

    pipe segment name in test

--- public/model/__tests__/pipeSegment.ts
+++ public/model/__tests__/pipeSegment.ts
@@ -4,231 +4,233 @@ import Valve from '../valve'
 
 describe('Constructor', () => {
 	it('should have a name', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.name).toBeTruthy()
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.name).toBeTruthy()
 	})
 
 	it('should have a length', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.length).not.toBeUndefined()
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.length).not.toBeUndefined()
 	})
 
 	it('should have a diameter', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.diameter).not.toBeUndefined()
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.diameter).not.toBeUndefined()
 	})
 
 	it('should have a mass flow rate', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.massFlow).not.toBeUndefined()
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.massFlow).not.toBeUndefined()
 	})
 
 	it('should have an in pressure', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.pressure.in).not.toBeUndefined()
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.pressure.in).not.toBeUndefined()
 	})
 
 	it('should have an out pressure', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.pressure.out).not.toBeUndefined()
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.pressure.out).not.toBeUndefined()
 	})
 
 	it('should have a source node', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.source).toBeInstanceOf(Node)
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.source).toBeInstanceOf(Node)
 	})
 
 	it('should have a destination node', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.destination).toBeInstanceOf(Node)
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.destination).toBeInstanceOf(Node)
 	})
 })
 
 describe('Constructor - receiving properties', () => {
 	it('should accept a name', () => {
-		const pipe = new PipeSegment({ name: 'michael' })
+		const pipeSeg = new PipeSegment({ name: 'michael' })
 
-		expect(pipe.name).toBe('michael')
+		expect(pipeSeg.name).toBe('michael')
 	})
 
 	it('should accept a length', () => {
-		const pipe = new PipeSegment({ length: 1 })
+		const pipeSeg = new PipeSegment({ length: 1 })
 
-		expect(pipe.length).toBe(1)
+		expect(pipeSeg.length).toBe(1)
 	})
 
 	it('should accept a diameter', () => {
-		const pipe = new PipeSegment({ diameter: 1 })
+		const pipeSeg = new PipeSegment({ diameter: 1 })
 
-		expect(pipe.diameter).toBe(1)
+		expect(pipeSeg.diameter).toBe(1)
 	})
 
 	it('should accept a massFlow value', () => {
-		const pipe = new PipeSegment({ massFlow: 1 })
+		const pipeSeg = new PipeSegment({ massFlow: 1 })
 
-		expect(pipe.massFlow).toBe(1)
+		expect(pipeSeg.massFlow).toBe(1)
 	})
 
 	it('should accept an endElevation value, which sets the elevation of the destination node', () => {
-		const pipe = new PipeSegment({ endElevation: 10 })
+		const pipeSeg = new PipeSegment({ endElevation: 10 })
 
-		expect(pipe.destination.elevation).toBe(10)
+		expect(pipeSeg.destination.elevation).toBe(10)
 	})
 
 	it('should set the destination x position to be the source x + length', () => {
-		const pipe = new PipeSegment({ length: 1 })
+		const pipeSeg = new PipeSegment({ length: 1 })
 
-		expect(pipe.destination.x).toBe(1)
+		expect(pipeSeg.destination.x).toBe(1)
 	})
+
+	it('should have a length equal to the distance between its source and destination nodes', () => {})
 })
 
 describe('Pressure', () => {
 	it('should have an input pressure the same as the source node', () => {
 		const node = new Node()
-		const pipe = new PipeSegment()
+		const pipeSeg = new PipeSegment()
 
 		node.pressure = 1
-		pipe.source = node
+		pipeSeg.source = node
 
-		expect(pipe.pressure.in).toBe(1)
+		expect(pipeSeg.pressure.in).toBe(1)
 	})
 
 	it('should update destination node pressure if out pressure is lower', () => {
 		const node = new Node()
-		const pipe = new PipeSegment()
+		const pipeSeg = new PipeSegment()
 
 		node.pressure = 2
-		pipe.pressure.out = 1
-		pipe.destination = node
+		pipeSeg.pressure.out = 1
+		pipeSeg.destination = node
 
 		expect(node.pressure).toBe(1)
 	})
 
 	it('should calculate pressure drop', () => {
 		const sourceNode = new Node({ pressure: 100000, temperature: 220 })
-		const pipe = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
-		pipe.source = sourceNode
+		const pipeSeg = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
+		pipeSeg.source = sourceNode
 
-		expect(pipe.pressure.out).toBeCloseTo(99999.97692)
+		expect(pipeSeg.pressure.out).toBeCloseTo(99999.97692)
 	})
 
 	it('should set the pressure of the destination node to the calculated value', () => {
 		const sourceNode = new Node({ pressure: 100000, temperature: 220 })
-		const pipe = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
-		pipe.source = sourceNode
+		const pipeSeg = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
+		pipeSeg.source = sourceNode
 
-		expect(pipe.destination.pressure).toBeCloseTo(99999.97692)
+		expect(pipeSeg.destination.pressure).toBeCloseTo(99999.97692)
 	})
 })
 
 describe('Pressure continuity', () => {
 	it('should return true if `pressure.out === destination.pressure`', () => {
 		const nodeA = new Node({ pressure: 100000, temperature: 220 })
-		const pipe1 = new PipeSegment({
+		const pipeSeg1 = new PipeSegment({
 			length: 200,
 			diameter: 2,
 			massFlow: 10,
 		})
-		pipe1.source = nodeA
+		pipeSeg1.source = nodeA
 
 		const nodeB = new Node({ pressure: 100000, temperature: 220 })
-		const pipe2 = new PipeSegment({
+		const pipeSeg2 = new PipeSegment({
 			length: 200,
 			diameter: 1,
 			massFlow: 10,
 		})
-		pipe2.source = nodeB
+		pipeSeg2.source = nodeB
 
 		const dest = new Node({ pressure: 100000, temperature: 220 })
-		pipe1.destination = dest
-		pipe2.destination = dest
+		pipeSeg1.destination = dest
+		pipeSeg2.destination = dest
 
-		expect(pipe2.pressureContinuity).toBe(true)
+		expect(pipeSeg2.pressureContinuity).toBe(true)
 	})
 
 	it('should return false if `pressure.out !== destination.pressure`', () => {
 		const nodeA = new Node({ pressure: 100000, temperature: 220 })
-		const pipe1 = new PipeSegment({
+		const pipeSeg1 = new PipeSegment({
 			length: 200,
 			diameter: 2,
 			massFlow: 10,
 		})
-		pipe1.source = nodeA
+		pipeSeg1.source = nodeA
 
 		const nodeB = new Node({ pressure: 100000, temperature: 220 })
-		const pipe2 = new PipeSegment({
+		const pipeSeg2 = new PipeSegment({
 			length: 200,
 			diameter: 1,
 			massFlow: 10,
 		})
-		pipe2.source = nodeB
+		pipeSeg2.source = nodeB
 
-		const dest = new Node({ pressure: 100000, temperature: 220 })
-		pipe1.destination = dest
-		pipe2.destination = dest
+		const dest = new Node({ temperature: 220 })
+		pipeSeg1.destination = dest
+		pipeSeg2.destination = dest
 
-		expect(pipe1.pressureContinuity).toBe(false)
+		expect(pipeSeg1.pressureContinuity).toBe(false)
 	})
 })
 
 describe('Direction', () => {
 	it('should return null by default', () => {
-		const pipe = new PipeSegment()
-		expect(pipe.direction).toBeNull()
+		const pipeSeg = new PipeSegment()
+		expect(pipeSeg.direction).toBeNull()
 	})
 
 	it('should return true if the source pressure is greater than the destination pressure', () => {
 		const sourceNode = new Node({ pressure: 100000, temperature: 220 })
-		const pipe = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
+		const pipeSeg = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
 
-		pipe.source = sourceNode
+		pipeSeg.source = sourceNode
 
-		expect(pipe.direction).toBe(true)
+		expect(pipeSeg.direction).toBe(true)
 	})
 
 	it('should return false if the source pressure is less than the destination pressure', () => {
 		const sourceNode = new Node({ pressure: 100000, temperature: 220 })
-		const pipe = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
+		const pipeSeg = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
 
-		pipe.source = sourceNode
-		pipe.pressure.out = 2 * pipe.pressure.in
+		pipeSeg.source = sourceNode
+		pipeSeg.pressure.out = 2 * pipeSeg.pressure.in
 
-		expect(pipe.direction).toBe(false)
+		expect(pipeSeg.direction).toBe(false)
 	})
 })
 
 describe('Valve', () => {
 	it('should return false by default', () => {
-		const pipe = new PipeSegment()
+		const pipeSeg = new PipeSegment()
 
-		expect(pipe.valve).toBe(false)
+		expect(pipeSeg.valve).toBe(false)
 	})
 
 	it('addValve should throw an error when given zero pressure', () => {
-		const pipe = new PipeSegment()
+		const pipeSeg = new PipeSegment()
 
-		expect(() => pipe.addValve()).toThrow('Invalid')
+		expect(() => pipeSeg.addValve()).toThrow('Invalid')
 	})
 
-	it('should create a valve when addValve is called on a pipe with a non-zero out pressure', () => {
+	it('should create a valve when addValve is called on a pipeSeg with a non-zero out pressure', () => {
 		const sourceNode = new Node({ pressure: 100000, temperature: 220 })
-		const pipe = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
-		pipe.source = sourceNode
+		const pipeSeg = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
+		pipeSeg.source = sourceNode
 
-		pipe.addValve()
+		pipeSeg.addValve()
 
-		expect(pipe.valve).toBeInstanceOf(Valve)
+		expect(pipeSeg.valve).toBeInstanceOf(Valve)
 	})
 
 	it('should reset to false when removeValve is called', () => {
 		const sourceNode = new Node({ pressure: 100000, temperature: 220 })
-		const pipe = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
-		pipe.source = sourceNode
+		const pipeSeg = new PipeSegment({ length: 200, diameter: 2, massFlow: 10 })
+		pipeSeg.source = sourceNode
 
-		pipe.addValve()
-		pipe.removeValve()
+		pipeSeg.addValve()
+		pipeSeg.removeValve()
 
-		expect(pipe.valve).toBe(false)
+		expect(pipeSeg.valve).toBe(false)
 	})
 
 	it('should be named after the pipe', () => {
@@ -236,17 +238,17 @@ describe('Valve', () => {
 			pressure: 100000,
 			temperature: 220,
 		})
-		const pipe = new PipeSegment({
+		const pipeSeg = new PipeSegment({
 			name: 'testpipe',
 			length: 200,
 			diameter: 2,
 			massFlow: 10,
 		})
-		pipe.source = sourceNode
+		pipeSeg.source = sourceNode
 
-		pipe.addValve()
+		pipeSeg.addValve()
 
-		expect((pipe.valve as Valve).name).toBe('testpipe-valve')
+		expect((pipeSeg.valve as Valve).name).toBe('testpipe-valve')
 	})
 
 	it('should have `pressure.in` equal to the `pressure.out` of the pipe', () => {
@@ -254,17 +256,17 @@ describe('Valve', () => {
 			pressure: 100000,
 			temperature: 220,
 		})
-		const pipe = new PipeSegment({
+		const pipeSeg = new PipeSegment({
 			name: 'testpipe',
 			length: 200,
 			diameter: 2,
 			massFlow: 10,
 		})
-		pipe.source = sourceNode
+		pipeSeg.source = sourceNode
 
-		pipe.addValve()
+		pipeSeg.addValve()
 
-		expect((pipe.valve as Valve).pressure.in).toEqual(pipe.pressure.out)
+		expect((pipeSeg.valve as Valve).pressure.in).toEqual(pipeSeg.pressure.out)
 	})
 
 	it('should have `pressure.out` equal to the `pressure` of the destination node', () => {
@@ -272,18 +274,18 @@ describe('Valve', () => {
 			pressure: 100000,
 			temperature: 220,
 		})
-		const pipe = new PipeSegment({
+		const pipeSeg = new PipeSegment({
 			name: 'testpipe',
 			length: 200,
 			diameter: 2,
 			massFlow: 10,
 		})
-		pipe.source = sourceNode
+		pipeSeg.source = sourceNode
 
-		pipe.addValve()
+		pipeSeg.addValve()
 
-		expect((pipe.valve as Valve).pressure.out).toEqual(
-			pipe.destination.pressure
+		expect((pipeSeg.valve as Valve).pressure.out).toEqual(
+			pipeSeg.destination.pressure
 		)
 	})
 })
