9c77fad5defe8a219487e84cfec7e99bf3b7288e
Jerell James <jerell1998@gmail.com>
2021-07-14 10:49:48 +0100

    button margin

--- components/input/button.tsx
+++ components/input/button.tsx
@@ -13,7 +13,7 @@ const Button = ({ fn, text = 'Submit' }: IButtonProps) => {
 	}
 	return (
 		<button
-			className='mt-4 mr-2 py-2 px-4 border border-transparent text-sm font-medium text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 w-24'
+			className='mt-2 mr-2 py-2 px-4 border border-transparent text-sm font-medium text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 w-24'
 			onClick={handleClick}
 		>
 			{text}
